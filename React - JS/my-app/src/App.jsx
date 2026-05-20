a625ddef546ffd13e47ad7e389cbb69ac91c07cb
AryaPatel0209 <aryapatel72186@gmail.com>
2026-05-20 14:27:46 +0530

    Day 8

---
+++ React - JS/my-app/src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+import Hello from "./hello"
+import reactLogo from './assets/hero.png'
+function App() {
+return(
+  <>
+  <h1>Hello Arya Stark</h1>
+  <Hello/>
+  <img src= {reactLogo} alt="React Logo" />
+  </>
+)
+}
+
+export default App
+
