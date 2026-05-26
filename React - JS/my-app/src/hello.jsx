4326889d7df992efd8a8db7b528c60803d4bb296
AryaPatel0209 <aryapatel72186@gmail.com>
2026-05-26 12:49:22 +0530

    day 8

--- React - JS/my-app/src/hello.jsx
+++ React - JS/my-app/src/hello.jsx
@@ -1,4 +1,24 @@
-function hello(){
-    return <h2> Hello World</h2>
+function hello() {
+    
+    const getName = (yourname) =>{
+        return yourname;
+    }
+    function handleClick(){
+        alert("Button was clicked")
+    }
+
+    const name = "Arya"
+    const name1 = "Priya"
+
+    return(
+        <>
+            <h1>Hello {getName(name)}</h1>
+            <h2>Bye {getName(name1)}</h2>
+            <button onClick ={handleClick}>Click Me</button>
+            <button onClick={()=> alert("Hello from Inline Function!")}>Say Hello</button>
+            
+        </>
+    )
 }
+
 export default hello
