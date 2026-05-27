a4234fa799dac217ef628fafc5d6f391acfd4df4
AryaPatel0209 <aryapatel72186@gmail.com>
2026-05-27 11:48:02 +0530

    ejs 2

--- Express-JS/index.js
+++ Express-JS/index.js
@@ -12,4 +12,31 @@ app.get("/adi", (req, res) => {
   res.send(`Panda baby `);
 });
 
+// app.get('/', (req,res) => {
+//   res.send(
+//    {
+//     name : "Arya",
+//     age : 20
+//    } 
+//   )
+// })
 
+// app.get('/', (req,res) => {
+//   res.send(
+//     ['Apple', 'Mango', 'Cherry']
+//   )
+// })
+
+// app.get('/', (req,res) => {
+//   res.send(
+//     {name : "Arya", age : 20}
+//   )
+// })
+
+app.get('/', (req,res) => {
+  const users = [
+      {id: 1, name : 'Arya'},
+      {id: 2, name : 'Priya'}
+  ]
+  res.json(users)
+})
